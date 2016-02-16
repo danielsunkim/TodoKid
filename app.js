28665627f7f90345ad91e671231f3c66047a097d
Daniel Kim <dkim2707@gmail.com>
2016-02-15 17:12:39 -0800

    Update working file

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-    $scope.name = 'Main';
-    
-}]);
 
-myApp.controller('secondController', ['$scope', function($scope) {
+// Custom directive with keypressEvents
+myApp.directive('keypressEvents', function ($document, $rootScope) {
+    return {
+        link: function () {
+            console.log('linked');
+            $document.bind('keypress', function (e) {
+                $rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
+            });
+        }
+    }
+});
+
+// Inject services into controllers
+myApp.controller('mainController', ['$scope', '$document', '$rootScope', function($scope, $document, $rootScope) {
     
-    $scope.name = 'Second';
-    
+    $scope.count = 0;
+    $scope.key = 'none';
+
+    // Increment function
+    $scope.increment = function () {
+      $scope.count++;      
+    }
+
+
+    // On rootScope, on keypress, apply this function with, scope.key is the key pressed
+    $rootScope.$on('keypress', function (evt, obj, key) {
+        $scope.$apply(function () {
+            $scope.key = key;
+            $scope.increment();
+            console.log(key)
+        });
+    });
+
+
+
+
 }]);
