fca18a1f7fa3bc13d12e02c40520ff400f58b78e
Daniel Kim <dkim2707@gmail.com>
2016-02-16 11:01:52 -0800

    Add jquery functionality of todo app

---
+++ todo.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+
+
+  // Select the li in box
+  $('.box').on("click", "li", function () {
+    $(this).toggleClass('completed');
+  });
+
+
+  // Remove the li when click on span in box class.
+  $('.box').on("click", " span", function (event) {
+    //Target parent of span, li, then fadeout and remove li.
+    $(this).parent().fadeOut(500, function () {
+      $(this).remove();
+    });
+    event.stopPropagation();
+  });
+
+  // listen for the keypress on the input
+  $('.box').on( "keypress", "input[type=text]", function (event) {
+    if ( event.which === 13 ) {
+      // save this value
+      var todoText = $(this).val();
+      $(this).val("")
+      $('.box ul').append('<li><span>X </span>'+todoText+'</li>')
+    }
+  });
+
+
+
+
+
+});
+
+
+
+
+
+
+  
