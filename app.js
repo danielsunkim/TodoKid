e8b31ccbb86d102774c9214777585e8da3531c70
Daniel Kim <dkim2707@gmail.com>
2016-02-15 22:06:33 -0800

    Implement add/remove class keyboard functionality

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 var myApp = angular.module('myApp', []);
-
-
-// Custom directive with keypressEvents
-// myApp.directive('keypressEvents', function ($document, $rootScope) {
-//     return {
-//         link: function () {
-//             console.log('linked');
-//             $document.bind('keypress', function (e) {
-//                 $rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
-//             });
-//         }
-//     }
-// });
-
 // Inject services into controllers: $scope, $document, and $rootScope
+var element = angular.element(document);
 
   var keyObj = {
     13: 'Enter',
@@ -23,6 +10,21 @@ var myApp = angular.module('myApp', []);
     9: 'Tab',
     8: 'Delete'
   }
+
+
+
+// angular.elemnt wraps a raw dom element or html string as a jQeruy element.
+element.on('keydown', function(event){
+  var keys = angular.element(document.getElementsByClassName(event.keyCode));
+  //grab the class name the keyCode class name, then add a class to it.
+  keys.addClass('down');
+});
+// angular.elemnt wraps a raw dom element or html string as a jQeruy element.
+element.on('keyup',function(event){
+  var keys = angular.element(document.getElementsByClassName(event.keyCode));
+  //grab the class name the keyCode class name, then remove a class from it.
+  keys.removeClass('down');
+});
 
 myApp.controller('mainController', ['$scope', '$document', '$rootScope', function($scope, $document, $rootScope) {
     
@@ -36,35 +38,25 @@ myApp.controller('mainController', ['$scope', '$document', '$rootScope', functio
     // Increment function
     $scope.increment = function () {
       $scope.count++;      
-    }
-
-    //Produces keyCode
-
-
-
-    // On rootScope, on keypress, apply this function with, scope.key is the key pressed
-    // $rootScope.$on('keypress', function (evt, obj, key) {
-    //     $scope.$apply(function () {
-    //         $scope.key = key;
-    //         $scope.increment();
-    //     });
-    // });
+    };
 
     $scope.keypress = function ($event) {
       var key = $event.keyCode;
       var string = String.fromCharCode(key);
       $scope.increment();
-      // String.fromCharCode($event.keyCode) convert into letter
+
       if ( keyObj[key] ) {
         $scope.lastKey = keyObj[key];
       } else {
         $scope.lastKey = string;
       }
-    }
+    };
 
+     
+
+    
 
 }]);
-
 
 
 
